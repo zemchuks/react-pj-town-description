eb42e3ea4c4df689808cff0b79a546aa39e68be8
Bruno Austin <brunoaugustine2@gmail.com>
2020-06-18 07:38:58 +0100

    initial commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,25 +4,25 @@ import logo from '../../logo.png'
 
 
 export default function Navbar() {
-    return <nav className="nav-wrapper purple">
-    <div className="container">
-    <img src={logo} alt="city tour logo" className="z-depth-0" />
+    return <nav className="nav-wrapper blue">
+        <div className="container">
+            <img src={logo} alt="city tour logo" className="z-depth-0" />
 
-    <a href="/" className="sidenav-trigger right" data-target="mobile-menu">
-    <i className="fa fa-navicon"></i></a>
-    
-    <a href="/" className="brand-logo">City Tours</a>
-    <ul className="right nav-links hide-on-med-and-down">
-    <li><a href="/" className="nav-link">Home</a></li>
-    <li><a href="/" className="nav-link">About</a></li>
-    <li><a href="/" className="nav-link active">Tours</a></li>
-    </ul>
-    <ul className="sidenav grey sidebar-mobile" id="sidebar-mobile">
-        <li><a href="/">Home</a></li>
-        <li><a href="/">About</a></li>
-        <li><a href="/">Tours</a></li>
-    </ul>
-    
-    </div>
+            <a href="/" className="sidenav-trigger right" data-target="mobile-menu">
+                <i className="fa fa-navicon"></i></a>
+
+            <a href="/" className="brand-logo">City Tours</a>
+            <ul className="right nav-links hide-on-med-and-down">
+                <li><a href="/" className="nav-link">Home</a></li>
+                <li><a href="/" className="nav-link">About</a></li>
+                <li><a href="/" className="nav-link active">Tours</a></li>
+            </ul>
+            <ul className="sidenav grey sidebar-mobile" id="sidebar-mobile">
+                <li><a href="/">Home</a></li>
+                <li><a href="/">About</a></li>
+                <li><a href="/">Tours</a></li>
+            </ul>
+
+        </div>
     </nav>
 }
